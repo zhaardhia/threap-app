69171cc9565f4d86250f03cda9f385449539e68f
Firzha Ardhia Ramadhan <firzharamadhan27@gmail.com>
2022-12-27 07:24:10 +0700

    tailwindcss

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-red-400">
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"

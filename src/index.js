cbd6ef90b2e5bf8fb5d6981bcd3ba6880b6231a4
Firzha Ardhia Ramadhan <firzharamadhan27@gmail.com>
2022-12-27 23:48:15 +0700

    wrapped apps with redux provider & react router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import store from './states';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function

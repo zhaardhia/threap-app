a06b9214d1eeb6fd106975c30ed2041f1647b1dd
Firzha Ardhia Ramadhan <firzharamadhan27@gmail.com>
2023-01-15 01:33:07 +0700

    remove unused import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import Sidebar from './Sidebar';
 
-const Navbar = ({ app }) => (
+const Navbar = () => (
   <div className="md:w-[50%] w-[80%] top-[2%] md:left-[25%] left-[10%] fixed h-[4rem] bg-[#FACFAD] shadow-lg rounded-2xl p-3 flex justify-between items-center z-10">
-    <Link to="/" className="text-2xl text-white">{app}</Link>
+    <Link to="/" className="text-2xl text-white">Threap</Link>
     <Sidebar />
   </div>
 );
-
-Navbar.propTypes = {
-  app: PropTypes.string.isRequired,
-};
 
 export default Navbar;
